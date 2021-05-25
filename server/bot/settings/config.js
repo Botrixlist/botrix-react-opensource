68cd240d4522c72129f1cc0d6b14352111f3aed2
Windows <massiopia@gmail.com>
2021-05-25 16:31:18 -0700

    Update config.js

--- server/bot/settings/config.js
+++ server/bot/settings/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  MONGO_STRING: "mongodb://209.216.85.138:65531/test",
+  MONGO_STRING: "",
   BOT_PREFIX: ".",
-  BOT_TOKEN: "NjUzMTE1MDgwMjY2ODc0ODgx.XeyS0Q.CwqRCJaXMUNHNbte8eB40Yb2jZw",
-  CLIENT_SECRET: "fyNv70ew2p8Dpf766h0jnfk7HMYyurUk",
-  CLIENT_ID: "653115080266874881",
-  GUILD_ID: "721282458708082713",
+  BOT_TOKEN: "",
+  CLIENT_SECRET: "",
+  CLIENT_ID: "",
+  GUILD_ID: "",
   DEV_IDS: [],
   STAFF_IDS: [],
 };
